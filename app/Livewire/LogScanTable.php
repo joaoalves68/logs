73348ffd55e7335af31b6261ebe90134343e9d12
João Alves <joao-alvespa@hotmail.com>
2025-08-20 17:12:07 -0300

    feat: log scan table using livewire

---
+++ app/Livewire/LogScanTable.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\LogScan;
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use Rappasoft\LaravelLivewireTables\Views\Column;
+
+class LogScanTable extends DataTableComponent
+{
+    protected $model = LogScan::class;
+
+    public function configure(): void
+    {
+        $this->setPrimaryKey('id');
+        $this->setTheme('tailwind');
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make("ID", "id")
+                ->sortable()
+                ->searchable(),
+
+            Column::make("Nome", "name")
+                ->sortable()
+                ->searchable(),
+
+            Column::make("Criado em", "created_at")
+                ->sortable(),
+        ];
+    }
+}
